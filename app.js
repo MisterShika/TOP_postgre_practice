bdabf310a6efeab66941b88ddfdfc51e12a0424c
Kyle W <125979751+MisterShika@users.noreply.github.com>
2025-03-04 22:41:13 +0900

    Fixing router/controller

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.render('index', { title: 'Hello, EJS!' });
+    res.render('index');
 });
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+    console.log('Server is running on http://localhost:3000');
 });
