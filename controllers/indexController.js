e7584a8b9fe53403a75a5dc254793eb828c90315
Kyle W <125979751+MisterShika@users.noreply.github.com>
2025-03-05 18:43:49 +0900

    Controller now appropriately passing DB data.

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -2,8 +2,10 @@ const db = require("../db/queries");
 
 async function getUsernames(req, res) {
     const usernames = await db.getAllUsernames();
-    console.log("Usernames: ", usernames);
-    res.send("Usernames: " + usernames.map(user => user.username).join(", "));
+    res.render("index", {
+        title: "User list",
+        users: usernames,
+    });
 }
 
 async function createUsernameGet(req, res) {
